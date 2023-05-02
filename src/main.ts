b76ef2c49c43c0cfba40a8767ac8f64c21b00382
itarek99 <tarekulislam7060@gmail.com>
2023-05-02 10:50:43 +0600

    basic types

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+/********** basic types ***********
+ *********************************/
+
+const userName: string = "Tarek";
+const a: number = 35;
+const b: number = 25;
+
+const sum = (a: number, b: number) => {
+  console.log(a + b);
+};
+
+sum(a, b);
+
+const re: RegExp = /\w+/g;
+
+/********** arrays & objects ***********
+ **************************************/
